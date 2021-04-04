b2f54de171c97cd70da20fa8a862d137532dae74
Tejas Mehta <tmthecoder@gmail.com>
2021-04-04 18:30:40 -0400

    add a navbar for the graph page

--- Edits/UserEdits.diffpack/UserModules/GraphCalc.playgroundmodule/Sources/GraphingCalculatorViewController.swift
+++ Edits/UserEdits.diffpack/UserModules/GraphCalc.playgroundmodule/Sources/GraphingCalculatorViewController.swift
@@ -3,26 +3,53 @@
 import UIKit
 
 public class GraphingCalculatorViewController : UIViewController {
-    var graphView = GraphView()
+    let graphView = GraphView()
+    let navigationBar = UINavigationBar()
+    
     public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        initializeNavbar()
         initializeGraph()
         graphView.currentGraph = Graph(expression: "HI")
+    }
+    
+    func initializeNavbar() {
+        view.backgroundColor = .systemBackground
+        // Create the actual item and add both right bar button items
+        let navigationItem = UINavigationItem(title: "Graph")
+        navigationBar.setItems([navigationItem], animated: false)
+        if navigationBar.superview == nil {
+            view.addSubview(navigationBar)
+        }
+        initializeNavbarConstraints()
     }
     
     func initializeGraph() {
         view.backgroundColor = .systemBackground
         if graphView.superview == nil {
-            view.addSubview(graphView)
+            view.insertSubview(graphView, belowSubview: navigationBar)
         }
         initializeGraphConstraints()
     }
+    
+    func initializeNavbarConstraints() {
+        navigationBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            navigationBar.topAnchor.constraint(equalTo: view.topAnchor),
+            navigationBar.leftAnchor.constraint(equalTo: view.leftAnchor),
+            navigationBar.rightAnchor.constraint(equalTo: view.rightAnchor),
+            navigationBar.heightAnchor.constraint(equalToConstant: 45)
+        ])
+    }
+    
     func initializeGraphConstraints() {
         guard let tabBar = tabBarController?.tabBar else {return}
         graphView.translatesAutoresizingMaskIntoConstraints = false
-        graphView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        graphView.bottomAnchor.constraint(equalTo: tabBar.topAnchor).isActive = true
-        graphView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        graphView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            graphView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor),
+            graphView.bottomAnchor.constraint(equalTo: tabBar.topAnchor),
+            graphView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            graphView.leftAnchor.constraint(equalTo: view.leftAnchor)
+        ])
     }
 }
